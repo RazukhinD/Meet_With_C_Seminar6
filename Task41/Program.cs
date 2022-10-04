99e1c167e2b52e7e7441e535162ed94abbdbc85c
Dmitrii <razuhindm@gmail.com>
2022-10-04 21:32:02 +0300

    Первая домашняя задача семинара 6

---
+++ Task41/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите какие-то  чисела через пробел: ");
+string[] numbers = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+int res = HowManyPositiveNumbers(numbers);
+WriteLine($"Количество элементов больше 0 = {res}");
+
+
+
+
+
+
+
+int HowManyPositiveNumbers(string[] array)
+{
+    int result = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (Convert.ToInt32(array[i])>0) result++;
+    }
+    return result;
+}
